cf54f09e2df19e4c133e2ecad1f7c104056a2354
Safin Ahmed <safin.ahmed2000@gmail.com>
2020-10-11 14:01:51 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,3 +11,38 @@ $(window).on('scroll' , function(){
 			
 		}
 });
+
+$(document).ready(function() {
+ 
+    setTimeout(function(){
+        $('body').addClass('loaded');
+     
+    }, 1000);
+
+
+ $('.heading, .get-in-touch-section h1').attr({
+
+ 	'data-aos': 'fade-up',
+ 	'data-aos-anchor-placement' : "top-bottom",
+ });
+
+ $('.social-icons ul li a').attr({
+
+ 	'data-aos' : "flip-up",
+ });
+
+  AOS.init(); 
+
+  $("body").niceScroll({
+   	  cursorcolor: "#de2b24",
+      cursorwidth: "10px",
+      cursorborder: "none",
+      zindex: [9999],
+      scrollspeed: 80,
+      mousescrollstep: 50,
+      horizrailenabled: false,
+});
+
+
+ 
+});
